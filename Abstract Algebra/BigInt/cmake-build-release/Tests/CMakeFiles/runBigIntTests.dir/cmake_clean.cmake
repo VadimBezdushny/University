c2e14bbe74e3266d8c7b47e4b7ac667b61f3241c
heimdall <vadim.bezdush@gmail.com>
2018-12-10 21:52:29 +0100

    Lab1

---
+++ Abstract Algebra/BigInt/cmake-build-release/Tests/CMakeFiles/runBigIntTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runBigIntTests.dir/InitTests.cpp.o"
+  "CMakeFiles/runBigIntTests.dir/ArithmeticTests.cpp.o"
+  "runBigIntTests.pdb"
+  "runBigIntTests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runBigIntTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Abstract Algebra/BigInt/cmake-build-release/Tests/lib/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/heimdall/Dev/University/Abstract Algebra/BigInt/Tests/lib/googletest/googletest
+# Build directory: /home/heimdall/Dev/University/Abstract Algebra/BigInt/cmake-build-release/Tests/lib/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
